2aefa5581df21de132cddffb90e74bc8d4f0d804
ZahidHasan4mJU <responsible.zahid@yahoo.com>
2014-09-25 20:03:21 +0600

    added

---
+++ DailyExpenses/DailyExpenses/Form1.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace DailyExpenses
+{
+    partial class ExpensesUIForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.amountTextBox = new System.Windows.Forms.TextBox();
+            this.categoryComboBox = new System.Windows.Forms.ComboBox();
+            this.particularTextBox = new System.Windows.Forms.TextBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.totalExpenseTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.maximumExpenseTextBox = new System.Windows.Forms.TextBox();
+            this.showButton = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.categoryComboBox2 = new System.Windows.Forms.ComboBox();
+            this.showButton2 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.totalTextBox = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.listView = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Amount";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Category";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Particular";
+            // 
+            // amountTextBox
+            // 
+            this.amountTextBox.Location = new System.Drawing.Point(84, 19);
+            this.amountTextBox.Name = "amountTextBox";
+            this.amountTextBox.Size = new System.Drawing.Size(140, 20);
+            this.amountTextBox.TabIndex = 3;
+            // 
+            // categoryComboBox
+            // 
+            this.categoryComboBox.FormattingEnabled = true;
+            this.categoryComboBox.Items.AddRange(new object[] {
+            "House Rent",
+            "Utility Bill",
+            "Conveyance",
+            "Grocery",
+            "Misc"});
+            this.categoryComboBox.Location = new System.Drawing.Point(84, 55);
+            this.categoryComboBox.Name = "categoryComboBox";
+            this.categoryComboBox.Size = new System.Drawing.Size(140, 21);
+            this.categoryComboBox.TabIndex = 4;
+            // 
+            // particularTextBox
+            // 
+            this.particularTextBox.Location = new System.Drawing.Point(84, 94);
+            this.particularTextBox.Name = "particularTextBox";
+            this.particularTextBox.Size = new System.Drawing.Size(140, 20);
+            this.particularTextBox.TabIndex = 5;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(140, 133);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(84, 30);
+            this.saveButton.TabIndex = 6;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.saveButton);
+            this.groupBox1.Controls.Add(this.particularTextBox);
+            this.groupBox1.Controls.Add(this.categoryComboBox);
+            this.groupBox1.Controls.Add(this.amountTextBox);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(3, 20);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(239, 184);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Daily Expense Enrtry";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Total Expense";
+            // 
+            // totalExpenseTextBox
+            // 
+            this.totalExpenseTextBox.Location = new System.Drawing.Point(105, 72);
+            this.totalExpenseTextBox.Name = "totalExpenseTextBox";
+            this.totalExpenseTextBox.Size = new System.Drawing.Size(118, 20);
+            this.totalExpenseTextBox.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(8, 106);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(99, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Maxumum Expense";
+            // 
+            // maximumExpenseTextBox
+            // 
+            this.maximumExpenseTextBox.Location = new System.Drawing.Point(105, 103);
+            this.maximumExpenseTextBox.Name = "maximumExpenseTextBox";
+            this.maximumExpenseTextBox.Size = new System.Drawing.Size(118, 20);
+            this.maximumExpenseTextBox.TabIndex = 11;
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(139, 35);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(84, 31);
+            this.showButton.TabIndex = 12;
+            this.showButton.Text = "Show";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.showButton);
+            this.groupBox2.Controls.Add(this.maximumExpenseTextBox);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.totalExpenseTextBox);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(4, 223);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(237, 153);
+            this.groupBox2.TabIndex = 13;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "View Summary";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 39);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Category";
+            // 
+            // categoryComboBox2
+            // 
+            this.categoryComboBox2.FormattingEnabled = true;
+            this.categoryComboBox2.Items.AddRange(new object[] {
+            "House Rent",
+            "Utility Bill",
+            "Conveyance",
+            "Grocery",
+            "Misc"});
+            this.categoryComboBox2.Location = new System.Drawing.Point(61, 36);
+            this.categoryComboBox2.Name = "categoryComboBox2";
+            this.categoryComboBox2.Size = new System.Drawing.Size(150, 21);
+            this.categoryComboBox2.TabIndex = 15;
+            // 
+            // showButton2
+            // 
+            this.showButton2.Location = new System.Drawing.Point(126, 66);
+            this.showButton2.Name = "showButton2";
+            this.showButton2.Size = new System.Drawing.Size(85, 30);
+            this.showButton2.TabIndex = 16;
+            this.showButton2.Text = "Show";
+            this.showButton2.UseVisualStyleBackColor = true;
+            this.showButton2.Click += new System.EventHandler(this.showButton2_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(58, 302);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(31, 13);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Total";
+            // 
+            // totalTextBox
+            // 
+            this.totalTextBox.Location = new System.Drawing.Point(109, 299);
+            this.totalTextBox.Name = "totalTextBox";
+            this.totalTextBox.Size = new System.Drawing.Size(102, 20);
+            this.totalTextBox.TabIndex = 19;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.listView);
+            this.groupBox3.Controls.Add(this.totalTextBox);
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Controls.Add(this.showButton2);
+            this.groupBox3.Controls.Add(this.categoryComboBox2);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Location = new System.Drawing.Point(248, 27);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(231, 348);
+            this.groupBox3.TabIndex = 20;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "View Category wise Expense";
+            // 
+            // listView
+            // 
+            this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView.FullRowSelect = true;
+            this.listView.GridLines = true;
+            this.listView.HideSelection = false;
+            this.listView.Location = new System.Drawing.Point(61, 102);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(150, 191);
+            this.listView.TabIndex = 20;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Category";
+            this.columnHeader1.Width = 78;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Particular";
+            this.columnHeader2.Width = 87;
+            // 
+            // ExpensesUIForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 403);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "ExpensesUIForm";
+            this.Text = "ExpensesUI";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox amountTextBox;
+        private System.Windows.Forms.ComboBox categoryComboBox;
+        private System.Windows.Forms.TextBox particularTextBox;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox totalExpenseTextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox maximumExpenseTextBox;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox categoryComboBox2;
+        private System.Windows.Forms.Button showButton2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox totalTextBox;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+    }
+}
+
---
+++ DailyExpenses/DailyExpenses/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CSVLib;
+
+namespace DailyExpenses
+{
+    public partial class ExpensesUIForm : Form
+    {
+        public ExpensesUIForm()
+        {
+            InitializeComponent();
+        }
+
+        private string fileLocation = @"D:\Expenses\dailyExpenses.csv";
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            FileStream aStream = new FileStream(fileLocation, FileMode.Append);
+            CsvFileWriter write = new CsvFileWriter(aStream);
+            List<string> list = new List<string>();
+            if (amountTextBox.Text != "" || categoryComboBox.Text != "" || particularTextBox.Text != "")
+            {
+                list.Add(amountTextBox.Text);
+                list.Add(categoryComboBox.Text);
+                list.Add(particularTextBox.Text);
+                write.WriteRow(list);
+                aStream.Close();
+                MessageBox.Show(@"success");
+            }
+
+
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            FileStream aStream2 = new FileStream(fileLocation, FileMode.OpenOrCreate);
+            CsvFileReader read = new CsvFileReader(aStream2);
+            List<string> list2 = new List<string>();
+
+            double num = 0;
+            double max = 0;
+            double totalExpense = 0;
+            while (read.ReadRow(list2))
+
+            {
+               totalExpense += Convert.ToDouble(list2[0]);
+                num = Convert.ToDouble(list2[0]);
+                if (max < num)
+                {
+                    max = num;
+                }
+
+
+            }
+            totalExpenseTextBox.Text = totalExpense.ToString();
+            maximumExpenseTextBox.Text = max.ToString();
+            
+            aStream2.Close();
+
+        }
+
+        private void showButton2_Click(object sender, EventArgs e)
+        {
+            FileStream aStream3 = new FileStream(fileLocation, FileMode.OpenOrCreate);
+            CsvFileReader read = new CsvFileReader(aStream3);
+            List<string> list = new List<string>();
+            double total = 0;
+            listView.Items.Clear();
+            while (read.ReadRow(list))
+           
+            {
+                if (list[1] == categoryComboBox2.Text)
+                {
+                    ListViewItem item = new ListViewItem(list[1]);
+                    item.SubItems.Add(list[2]);
+                    listView.Items.Add(item);
+                    total += Convert.ToDouble(list[0]);
+                } 
+                  
+                 
+                  
+
+            }
+            totalTextBox.Text = total.ToString();
+            aStream3.Close();
+
+        }
+    }
+}
